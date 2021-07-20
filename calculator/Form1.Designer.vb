5130aceba89e08d1feab443bbd2cc17789594e3b
techindia2010 <77503999+techindia2010@users.noreply.github.com>
2021-07-20 19:48:55 +0530

    Add files via upload

---
+++ calculator/Form1.Designer.vb
@@ -0,0 +1,451 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.negate = New System.Windows.Forms.Button()
+        Me.zero = New System.Windows.Forms.Button()
+        Me.decimal_point = New System.Windows.Forms.Button()
+        Me.equal_to_sign = New System.Windows.Forms.Button()
+        Me.plus = New System.Windows.Forms.Button()
+        Me.three = New System.Windows.Forms.Button()
+        Me.two = New System.Windows.Forms.Button()
+        Me.one = New System.Windows.Forms.Button()
+        Me.minus = New System.Windows.Forms.Button()
+        Me.six = New System.Windows.Forms.Button()
+        Me.five = New System.Windows.Forms.Button()
+        Me.four = New System.Windows.Forms.Button()
+        Me.multiply = New System.Windows.Forms.Button()
+        Me.nine = New System.Windows.Forms.Button()
+        Me.eight = New System.Windows.Forms.Button()
+        Me.seven = New System.Windows.Forms.Button()
+        Me.inverse = New System.Windows.Forms.Button()
+        Me.square = New System.Windows.Forms.Button()
+        Me.square_root = New System.Windows.Forms.Button()
+        Me.division = New System.Windows.Forms.Button()
+        Me.c = New System.Windows.Forms.Button()
+        Me.ce = New System.Windows.Forms.Button()
+        Me.percentage = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.memory_recall = New System.Windows.Forms.Button()
+        Me.memory_clear = New System.Windows.Forms.Button()
+        Me.cube_root = New System.Windows.Forms.Button()
+        Me.cube = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.memory = New System.Windows.Forms.Button()
+        Me.memory_store = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.btn_backspace = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'negate
+        '
+        Me.negate.Location = New System.Drawing.Point(2, 647)
+        Me.negate.Name = "negate"
+        Me.negate.Size = New System.Drawing.Size(112, 70)
+        Me.negate.TabIndex = 2
+        Me.negate.Text = "±"
+        Me.negate.UseVisualStyleBackColor = True
+        '
+        'zero
+        '
+        Me.zero.Location = New System.Drawing.Point(120, 647)
+        Me.zero.Name = "zero"
+        Me.zero.Size = New System.Drawing.Size(112, 70)
+        Me.zero.TabIndex = 3
+        Me.zero.Text = "0"
+        Me.zero.UseVisualStyleBackColor = True
+        '
+        'decimal_point
+        '
+        Me.decimal_point.Location = New System.Drawing.Point(238, 647)
+        Me.decimal_point.Name = "decimal_point"
+        Me.decimal_point.Size = New System.Drawing.Size(112, 70)
+        Me.decimal_point.TabIndex = 4
+        Me.decimal_point.Text = "."
+        Me.decimal_point.UseVisualStyleBackColor = True
+        '
+        'equal_to_sign
+        '
+        Me.equal_to_sign.Location = New System.Drawing.Point(356, 647)
+        Me.equal_to_sign.Name = "equal_to_sign"
+        Me.equal_to_sign.Size = New System.Drawing.Size(112, 70)
+        Me.equal_to_sign.TabIndex = 5
+        Me.equal_to_sign.Text = "="
+        Me.equal_to_sign.UseVisualStyleBackColor = True
+        '
+        'plus
+        '
+        Me.plus.Location = New System.Drawing.Point(356, 571)
+        Me.plus.Name = "plus"
+        Me.plus.Size = New System.Drawing.Size(112, 70)
+        Me.plus.TabIndex = 9
+        Me.plus.Text = "+"
+        Me.plus.UseVisualStyleBackColor = True
+        '
+        'three
+        '
+        Me.three.Location = New System.Drawing.Point(238, 571)
+        Me.three.Name = "three"
+        Me.three.Size = New System.Drawing.Size(112, 70)
+        Me.three.TabIndex = 8
+        Me.three.Text = "3"
+        Me.three.UseVisualStyleBackColor = True
+        '
+        'two
+        '
+        Me.two.Location = New System.Drawing.Point(120, 571)
+        Me.two.Name = "two"
+        Me.two.Size = New System.Drawing.Size(112, 70)
+        Me.two.TabIndex = 7
+        Me.two.Text = "2"
+        Me.two.UseVisualStyleBackColor = True
+        '
+        'one
+        '
+        Me.one.Location = New System.Drawing.Point(2, 571)
+        Me.one.Name = "one"
+        Me.one.Size = New System.Drawing.Size(112, 70)
+        Me.one.TabIndex = 6
+        Me.one.Text = "1"
+        Me.one.UseVisualStyleBackColor = True
+        '
+        'minus
+        '
+        Me.minus.Location = New System.Drawing.Point(356, 495)
+        Me.minus.Name = "minus"
+        Me.minus.Size = New System.Drawing.Size(112, 70)
+        Me.minus.TabIndex = 13
+        Me.minus.Text = "-"
+        Me.minus.UseVisualStyleBackColor = True
+        '
+        'six
+        '
+        Me.six.Location = New System.Drawing.Point(238, 495)
+        Me.six.Name = "six"
+        Me.six.Size = New System.Drawing.Size(112, 70)
+        Me.six.TabIndex = 12
+        Me.six.Text = "6"
+        Me.six.UseVisualStyleBackColor = True
+        '
+        'five
+        '
+        Me.five.Location = New System.Drawing.Point(120, 495)
+        Me.five.Name = "five"
+        Me.five.Size = New System.Drawing.Size(112, 70)
+        Me.five.TabIndex = 11
+        Me.five.Text = "5"
+        Me.five.UseVisualStyleBackColor = True
+        '
+        'four
+        '
+        Me.four.Location = New System.Drawing.Point(2, 495)
+        Me.four.Name = "four"
+        Me.four.Size = New System.Drawing.Size(112, 70)
+        Me.four.TabIndex = 10
+        Me.four.Text = "4"
+        Me.four.UseVisualStyleBackColor = True
+        '
+        'multiply
+        '
+        Me.multiply.Location = New System.Drawing.Point(356, 419)
+        Me.multiply.Name = "multiply"
+        Me.multiply.Size = New System.Drawing.Size(112, 70)
+        Me.multiply.TabIndex = 17
+        Me.multiply.Text = "×"
+        Me.multiply.UseVisualStyleBackColor = True
+        '
+        'nine
+        '
+        Me.nine.Location = New System.Drawing.Point(238, 419)
+        Me.nine.Name = "nine"
+        Me.nine.Size = New System.Drawing.Size(112, 70)
+        Me.nine.TabIndex = 16
+        Me.nine.Text = "9"
+        Me.nine.UseVisualStyleBackColor = True
+        '
+        'eight
+        '
+        Me.eight.Location = New System.Drawing.Point(120, 419)
+        Me.eight.Name = "eight"
+        Me.eight.Size = New System.Drawing.Size(112, 70)
+        Me.eight.TabIndex = 15
+        Me.eight.Text = "8"
+        Me.eight.UseVisualStyleBackColor = True
+        '
+        'seven
+        '
+        Me.seven.Location = New System.Drawing.Point(2, 419)
+        Me.seven.Name = "seven"
+        Me.seven.Size = New System.Drawing.Size(112, 70)
+        Me.seven.TabIndex = 14
+        Me.seven.Text = "7"
+        Me.seven.UseVisualStyleBackColor = True
+        '
+        'inverse
+        '
+        Me.inverse.Location = New System.Drawing.Point(2, 343)
+        Me.inverse.Name = "inverse"
+        Me.inverse.Size = New System.Drawing.Size(112, 70)
+        Me.inverse.TabIndex = 18
+        Me.inverse.Text = "¹/ₓ"
+        Me.inverse.UseVisualStyleBackColor = True
+        '
+        'square
+        '
+        Me.square.Location = New System.Drawing.Point(120, 343)
+        Me.square.Name = "square"
+        Me.square.Size = New System.Drawing.Size(112, 70)
+        Me.square.TabIndex = 19
+        Me.square.Text = "x²"
+        Me.square.UseVisualStyleBackColor = True
+        '
+        'square_root
+        '
+        Me.square_root.Location = New System.Drawing.Point(238, 343)
+        Me.square_root.Name = "square_root"
+        Me.square_root.Size = New System.Drawing.Size(112, 70)
+        Me.square_root.TabIndex = 20
+        Me.square_root.Text = "²√x"
+        Me.square_root.UseVisualStyleBackColor = True
+        '
+        'division
+        '
+        Me.division.Location = New System.Drawing.Point(356, 343)
+        Me.division.Name = "division"
+        Me.division.Size = New System.Drawing.Size(112, 70)
+        Me.division.TabIndex = 21
+        Me.division.Text = "÷"
+        Me.division.UseVisualStyleBackColor = True
+        '
+        'c
+        '
+        Me.c.Location = New System.Drawing.Point(238, 267)
+        Me.c.Name = "c"
+        Me.c.Size = New System.Drawing.Size(112, 70)
+        Me.c.TabIndex = 24
+        Me.c.Text = "C"
+        Me.c.UseVisualStyleBackColor = True
+        '
+        'ce
+        '
+        Me.ce.Location = New System.Drawing.Point(120, 267)
+        Me.ce.Name = "ce"
+        Me.ce.Size = New System.Drawing.Size(112, 70)
+        Me.ce.TabIndex = 23
+        Me.ce.Text = "CE"
+        Me.ce.UseVisualStyleBackColor = True
+        '
+        'percentage
+        '
+        Me.percentage.Location = New System.Drawing.Point(2, 267)
+        Me.percentage.Name = "percentage"
+        Me.percentage.Size = New System.Drawing.Size(112, 70)
+        Me.percentage.TabIndex = 22
+        Me.percentage.Text = "%"
+        Me.percentage.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(2, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(302, 25)
+        Me.Label2.TabIndex = 27
+        Me.Label2.Text = "This calculator is made by Jatan Patel"
+        '
+        'memory_recall
+        '
+        Me.memory_recall.Location = New System.Drawing.Point(356, 191)
+        Me.memory_recall.Name = "memory_recall"
+        Me.memory_recall.Size = New System.Drawing.Size(112, 70)
+        Me.memory_recall.TabIndex = 31
+        Me.memory_recall.Text = "MR"
+        Me.memory_recall.UseVisualStyleBackColor = True
+        '
+        'memory_clear
+        '
+        Me.memory_clear.Location = New System.Drawing.Point(238, 191)
+        Me.memory_clear.Name = "memory_clear"
+        Me.memory_clear.Size = New System.Drawing.Size(112, 70)
+        Me.memory_clear.TabIndex = 30
+        Me.memory_clear.Text = "MC"
+        Me.memory_clear.UseVisualStyleBackColor = True
+        '
+        'cube_root
+        '
+        Me.cube_root.Location = New System.Drawing.Point(120, 191)
+        Me.cube_root.Name = "cube_root"
+        Me.cube_root.Size = New System.Drawing.Size(112, 70)
+        Me.cube_root.TabIndex = 29
+        Me.cube_root.Text = "³√x"
+        Me.cube_root.UseVisualStyleBackColor = True
+        '
+        'cube
+        '
+        Me.cube.Location = New System.Drawing.Point(2, 191)
+        Me.cube.Name = "cube"
+        Me.cube.Size = New System.Drawing.Size(112, 70)
+        Me.cube.TabIndex = 28
+        Me.cube.Text = "³"
+        Me.cube.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox1.Location = New System.Drawing.Point(2, 48)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(0)
+        Me.TextBox1.MaxLength = 32768
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(466, 48)
+        Me.TextBox1.TabIndex = 32
+        Me.TextBox1.Text = "0"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'memory
+        '
+        Me.memory.Location = New System.Drawing.Point(356, 139)
+        Me.memory.Name = "memory"
+        Me.memory.Size = New System.Drawing.Size(112, 43)
+        Me.memory.TabIndex = 36
+        Me.memory.Text = "M"
+        Me.memory.UseVisualStyleBackColor = True
+        '
+        'memory_store
+        '
+        Me.memory_store.Location = New System.Drawing.Point(238, 139)
+        Me.memory_store.Name = "memory_store"
+        Me.memory_store.Size = New System.Drawing.Size(112, 43)
+        Me.memory_store.TabIndex = 35
+        Me.memory_store.Text = "MS"
+        Me.memory_store.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(120, 139)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(112, 43)
+        Me.Button3.TabIndex = 34
+        Me.Button3.Text = "M-"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(2, 139)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(112, 43)
+        Me.Button4.TabIndex = 33
+        Me.Button4.Text = "M+"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'btn_backspace
+        '
+        Me.btn_backspace.Location = New System.Drawing.Point(356, 267)
+        Me.btn_backspace.Name = "btn_backspace"
+        Me.btn_backspace.Size = New System.Drawing.Size(112, 70)
+        Me.btn_backspace.TabIndex = 37
+        Me.btn_backspace.Text = "Backspace"
+        Me.btn_backspace.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(473, 718)
+        Me.Controls.Add(Me.btn_backspace)
+        Me.Controls.Add(Me.memory)
+        Me.Controls.Add(Me.memory_store)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.memory_recall)
+        Me.Controls.Add(Me.memory_clear)
+        Me.Controls.Add(Me.cube_root)
+        Me.Controls.Add(Me.cube)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.c)
+        Me.Controls.Add(Me.ce)
+        Me.Controls.Add(Me.percentage)
+        Me.Controls.Add(Me.division)
+        Me.Controls.Add(Me.square_root)
+        Me.Controls.Add(Me.square)
+        Me.Controls.Add(Me.inverse)
+        Me.Controls.Add(Me.multiply)
+        Me.Controls.Add(Me.nine)
+        Me.Controls.Add(Me.eight)
+        Me.Controls.Add(Me.seven)
+        Me.Controls.Add(Me.minus)
+        Me.Controls.Add(Me.six)
+        Me.Controls.Add(Me.five)
+        Me.Controls.Add(Me.four)
+        Me.Controls.Add(Me.plus)
+        Me.Controls.Add(Me.three)
+        Me.Controls.Add(Me.two)
+        Me.Controls.Add(Me.one)
+        Me.Controls.Add(Me.equal_to_sign)
+        Me.Controls.Add(Me.decimal_point)
+        Me.Controls.Add(Me.zero)
+        Me.Controls.Add(Me.negate)
+        Me.Name = "Form1"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents negate As Button
+    Friend WithEvents zero As Button
+    Friend WithEvents decimal_point As Button
+    Friend WithEvents equal_to_sign As Button
+    Friend WithEvents plus As Button
+    Friend WithEvents three As Button
+    Friend WithEvents two As Button
+    Friend WithEvents one As Button
+    Friend WithEvents minus As Button
+    Friend WithEvents six As Button
+    Friend WithEvents five As Button
+    Friend WithEvents four As Button
+    Friend WithEvents multiply As Button
+    Friend WithEvents nine As Button
+    Friend WithEvents eight As Button
+    Friend WithEvents seven As Button
+    Friend WithEvents inverse As Button
+    Friend WithEvents square As Button
+    Friend WithEvents square_root As Button
+    Friend WithEvents division As Button
+    Friend WithEvents c As Button
+    Friend WithEvents ce As Button
+    Friend WithEvents percentage As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents memory_recall As Button
+    Friend WithEvents memory_clear As Button
+    Friend WithEvents cube_root As Button
+    Friend WithEvents cube As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents memory As Button
+    Friend WithEvents memory_store As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents btn_backspace As Button
+End Class
---
+++ calculator/Form1.vb
@@ -0,0 +1,162 @@
+﻿Public Class Form1
+    Dim no1 As Decimal
+    Dim no2 As Decimal
+    Dim op As Integer = 0 '8=add, 1=subtract, 2=multiply, 3=division, 4=square root, 5=square, 6=cube 7=cube root
+    Dim result As Decimal
+    Dim op_pressed As Boolean
+    Dim subString As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles negate.Click
+        If TextBox1.Text <> "0" Then
+            If Not TextBox1.Text.Contains("-") Then
+                TextBox1.Text = "-" + TextBox1.Text
+            Else
+                TextBox1.Text = Mid(TextBox1.Text, 2)
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MessageBox.Show("Click on 'OK' to delete this app.")
+    End Sub
+    Private Sub one_Click(sender As Object, e As EventArgs) Handles one.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text = TextBox1.Text + "1"
+        ElseIf TextBox1.Text = "0" Then
+            TextBox1.Text = "1"
+        End If
+    End Sub
+
+    Private Sub two_Click(sender As Object, e As EventArgs) Handles two.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text = TextBox1.Text + "2"
+        ElseIf TextBox1.Text = "0" Then
+            TextBox1.Text = "2"
+        End If
+    End Sub
+
+    Private Sub three_Click(sender As Object, e As EventArgs) Handles three.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text = TextBox1.Text + "3"
+        ElseIf TextBox1.Text = "0" Then
+            TextBox1.Text = "3"
+        End If
+    End Sub
+
+    Private Sub four_Click(sender As Object, e As EventArgs) Handles four.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text = TextBox1.Text + "4"
+        ElseIf TextBox1.Text = "0" Then
+            TextBox1.Text = "4"
+        End If
+    End Sub
+
+    Private Sub five_Click(sender As Object, e As EventArgs) Handles five.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text = TextBox1.Text + "5"
+        ElseIf TextBox1.Text = "0" Then
+            TextBox1.Text = "5"
+        End If
+    End Sub
+
+    Private Sub six_Click(sender As Object, e As EventArgs) Handles six.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text = TextBox1.Text + "6"
+        ElseIf TextBox1.Text = "0" Then
+            TextBox1.Text = "6"
+        End If
+    End Sub
+
+    Private Sub seven_Click(sender As Object, e As EventArgs) Handles seven.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text = TextBox1.Text + "7"
+        ElseIf TextBox1.Text = "0" Then
+            TextBox1.Text = "7"
+        End If
+    End Sub
+
+    Private Sub eight_Click(sender As Object, e As EventArgs) Handles eight.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text = TextBox1.Text + "8"
+        ElseIf TextBox1.Text = "0" Then
+            TextBox1.Text = "8"
+        End If
+    End Sub
+
+    Private Sub nine_Click(sender As Object, e As EventArgs) Handles nine.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text = TextBox1.Text + "9"
+        ElseIf TextBox1.Text = "0" Then
+            TextBox1.Text = "9"
+        End If
+    End Sub
+
+    Private Sub multiply_Click(sender As Object, e As EventArgs) Handles multiply.Click
+
+    End Sub
+
+    Private Sub inverse_Click(sender As Object, e As EventArgs) Handles inverse.Click
+
+    End Sub
+
+    Private Sub square_Click(sender As Object, e As EventArgs) Handles square.Click
+        TextBox1.Text = TextBox1.Text * TextBox1.Text
+    End Sub
+
+
+    Private Sub division_Click(sender As Object, e As EventArgs) Handles division.Click
+
+    End Sub
+
+    Private Sub square_root_Click(sender As Object, e As EventArgs) Handles square_root.Click
+
+    End Sub
+
+    Private Sub decimal_point_Click(sender As Object, e As EventArgs) Handles decimal_point.Click
+        If Not TextBox1.Text.Contains(".") Then
+            TextBox1.Text = TextBox1.Text + "."
+        End If
+    End Sub
+
+    Private Sub zero_Click(sender As Object, e As EventArgs) Handles zero.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text = TextBox1.Text + "0"
+        End If
+    End Sub
+
+    Private Sub btn_backspace_Click(sender As Object, e As EventArgs) Handles btn_backspace.Click
+        If TextBox1.Text.Length = 1 Then
+            TextBox1.Text = "0"
+        ElseIf TextBox1.Text.Length = 2 And TextBox1.Text.Contains("-") Then
+            TextBox1.Text = "0"
+        ElseIf TextBox1.Text <> "0" Then
+            TextBox1.Text = Microsoft.VisualBasic.Strings.Left(TextBox1.Text, TextBox1.Text.Length - 1)
+        End If
+    End Sub
+
+    Private Sub ce_Click(sender As Object, e As EventArgs) Handles ce.Click
+        TextBox1.Text = "0"
+    End Sub
+
+    Private Sub plus_Click(sender As Object, e As EventArgs) Handles plus.Click
+        TextBox1.Text = "0"
+        If op_pressed = True Then
+            calculate(no1, Val(TextBox1.Text), op)
+        End If
+        no1 = Val(TextBox1.Text)
+        op = 8
+        op_pressed = True
+    End Sub
+
+    Private Sub equal_to_sign_Click(sender As Object, e As EventArgs) Handles equal_to_sign.Click
+        no2 = Val(TextBox1.Text)
+        calculate(no1, no2, op)
+    End Sub
+    Private Sub calculate(digi1, digi2, oper)
+        If oper = 8 Then
+            TextBox1.Text = Val(digi1) + Val(digi2)
+            no1 = TextBox1.Text
+            op = 0
+        End If
+    End Sub
+
+End Class
